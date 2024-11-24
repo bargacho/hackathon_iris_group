1d7e80edf4b3d6e09a67e318c05972482fed61f7
slivering <47292273+slivering@users.noreply.github.com>
2024-11-24 23:15:57 +0100

    Ajouter une mémoire des messages
    FIXME: L'historique des messages n'est pas affiché

--- iris/prompting.py
+++ iris/prompting.py
@@ -9,33 +9,41 @@ CHATBOT = ChatMistralAI(
     model="open-mistral-7b",
     temperature=0.7,
     max_retries=2,
-    max_tokens=256,
+    max_tokens=1024,
     api_key=MISTRAL_API_KEY
 )
 
+MESSAGE_HISTORY = [
+    (
+        "system",
+        "You are an AI assistant trained to help the user making a purchase in the online plant shop. "
+        "Be welcoming and assist the user by managing the customer order. "
+        "Only suggest products if the user is asking for it. "
+        "Always take the conversation flow into account."
+    )
+]
 
 def run_model(input):
     # Cf RAG codée plus haut, pour trouver le produit approprié
     context = search_plants(input)
 
     # modèle de prompt (`context` est le résultat de la RAG)
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "You are an AI assistant trained to recommend plants based on user requirements. "
-                "Give recommendations based on these products: {context}"
-            ),
-            ("human", "{input}"),
-        ]
-    )
+    prompt = ChatPromptTemplate.from_messages(MESSAGE_HISTORY + [
+        ("human", "{input}"),
+        (
+            "system",
+            "If the user just asked for a plant recommendation, answer based on these products: {context}"
+        ),
+    ])
+    MESSAGE_HISTORY.append(("human", f"{input}"))
     chain = prompt | CHATBOT
 
-    # TODO: add images + check if user is asking for a recommendation
-    return chain.invoke({
+    response = chain.invoke({
         'context': context,
         'input': input,
     })
+    MESSAGE_HISTORY.append(("assistant", response.content))
+    return response
 
 
 
